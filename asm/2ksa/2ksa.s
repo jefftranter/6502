ee10da0c4557610745749443dbf18269693f2992
Jeff Tranter <tranter@pobox.com>
2012-04-24 20:53:11 -0400

    Check in more code.

--- asm/2ksa/2ksa.s
+++ asm/2ksa/2ksa.s
@@ -3,13 +3,48 @@
 ; by Robert Ford Denison
 
 ; Global Symbols on Page Zero
-IOBUF    = $00           ; I/O Buffer  prompt or command field.
+IOBUF		= $00	 	; I/O Buffer; prompt or command field.
 LABEL    = $07           ; I/O buffer; label field.
 OPCODE   = $0E           ; I/O buffer; opcode field.
 OPRAND   = $15           ; I/O buffer; operand field.
 USER     = $23           ; Six bytes available for use by user commands.
 ADL      = $29           ; Low address pointer for various subroutines.
 ADH      = $2A           ; High address pointer.
+MISCL    = $2B           ; Miscellaneous uses.
+MISCH    = $2C           ; Ditto.
+TEMP     = $2D           ; Various temporary uses.
+MNE      = $2E           ; Mnemonic code.
+BYTES    = $2F           ; Lengths of lines, etc.
+TBL      = $30           ; Low address pointer for table; used by MATCH.
+TBH      = $31           ; High address pointer (Subroutine MATCH).
+RFL      = $32           ; Low address pointer for string to be matched.
+RFH      = $33           ; High address pointer (MATCH).
+LEN      = $34           ; Length of each record in table (MATCH).
+HBC      = $35           ; Number of highest bye in record which must match.
+NUM      = $36           ; Number of highest record in table (MATCH).
+OPCPTR   = $37           ; Pointer to opcode in OPCTAB.
+PRNTOK   = $38           ; Flag to enable printing by Subroutine PRNTCK.
+WRONG    = $39           ; Flag for illegal line numbers (PRNTCK).
+MODE     = $3A           ; Code for address mode.
+SAVX     = $3B           ; Used to preserve X register.
+GLOBAL   = $3C           ; Number of last global symbol.
+PRGLEN   = $3D           ; Length of source code.
+CRNTAL   = $3E           ; Low address pointer to current source code line.
+CRNTAH   = $3F           ; High address pointer.
+MDLADL   = $40           ; Module pointer, low address.
+MDLADH   = $41           ; Module pointer, high address.
+MNETBL   = $42           ; Parameters for MNETAB (see TBL to NUM above).
+MODTBL   = $49           ; Parameters for MODTAB.
+SYMTBL   = $50           ; Low address pointer to last entry in symbol table.
+SYMTBH   = $51           ; High address pointer.
+SYMRFL   = $52           ; Low address pointer for symbol to be compared.
+SYMRFH   = $53           ; High address pointer.
+SYMNUM   = $56           ; Number of last symbol.
+OBJECT   = $57           ; Low address pointer to object code.
+OBJCT1   = $58           ; High address pointer.
+FIRST    = $59           ; First line in range for print (PRNTCK).
+LAST     = $5A           ; First line after print range.
+LAST2    = $5B           ; High order address; same as CRNTAH.
 
          .org $0200
 MNETAB:                  ; Three-character ASCII mnemonics for instructions
@@ -94,15 +129,150 @@ MAX:                         ; Lowest illegal value of MNE for each mode.
 BASE:                        ; Base value for mode added to MNE to get OPCPTR
          .byte $00,$F2,$04,$11,$22,$35,$32,$3A,$31,$50,$63,$75,$6E
 
-PRMTAB:
-         .byte $0C,$80,$0C,$A5,$02,$0E,$00
+PRMTAB:  ; Initialization values for CRNTAH through SYMNUM.
+         .byte $0C,$80,$0C,$A5,$02,$0E,$00,$03,$02,$37,$C0,$02,$11,$00,$02,$01,$0C,$F8,$09,$15,$00,$08,$05,$08
 
-OPCTAB:
+USRPRM:  ; Four bytes available for user parameters.
+        .byte $FF,$FF,$FF,$FF
 
+OPCTAB:  ; Machine language opcodes pointed to by OPCPTR
+        .byte $00,$18,$D8,$58,$B8,$CA,$88,$E8,$C8,$EA,$48
+        .byte $08,$68,$28,$40,$60,$38,$F8,$78,$AA,$A8,$BA,$BA,$9A,$98,$0A,$4A
+        .byte $2A,$6A,$E0,$FF,$A2,$C0,$A0,$FF,$69,$29,$C9,$49,$A9,$09,$E9,$E4
+        .byte $86,$A6,$C4,$A4,$84,$65,$25,$C5,$45,$A5,$05,$E5,$85,$06,$46,$26
+        .byte $66,$C6,$E6,$24,$B4,$94,$75,$35,$D5,$55,$B5,$15,$F5,$95,$16,$56
+        .byte $36,$76,$D6,$F6,$B6,$96,$61,$21,$C1,$41,$A1,$01,$E1,$81,$71,$31
+        .byte $D1,$51,$B1,$11,$F1,$91,$90,$B0,$F0,$30,$D0,$10,$50,$70,$EC,$8E
+        .byte $AE,$CC,$AC,$8C,$6D,$2D,$CD,$4D,$AD,$0D,$ED,$8D,$0E,$4E,$2E,$6E
+        .byte $CE,$EE,$2C,$4C,$20,$BC,$FF,$7D,$3D,$DD,$5D,$BD,$1D,$FD,$9D,$1E
+        .byte $5E,$3E,$7E,$DE,$FE,$BE,$FF,$FF,$FF,$79,$39,$D9,$59,$B9,$19,$F9
+        .byte $99,$6C,$FF
+        
 ; Subroutine MATCH. Search table for match to reference, X points to
 ; search parameters on page zero. Sets z if match found, returns
 ; number of matching record in X.
-MATCH:
+	.proc	MATCH
         STX     ADL             ; Put address of
-        LDX     #$00            ; search parameter
+        LDX     #0              ; search parameter
         STX     ADH             ; list in ADL, H.
+        LDY     #6              
+PARAM:  LDA     (ADL),Y         ; Move parameters
+        STA     TBL,Y           ; to workspace.
+        DEY
+        BPL     PARAM
+        LDX     NUM             ; Compare X records
+RECORD: LDY     HBC
+BYTE:   LDA     (TBL),Y         ; First Y+1 bytes
+        CMP     (RFL),Y         ; must match.
+        BEQ     OK
+        LDY     #$FF            ; Mismatch.
+ OK:    DEY
+        BPL     BYTE
+        INY                     ; All ok?
+        BNE     INCADR
+        RTS
+INCADR: SEC                     ; z set.
+        LDA     TBL             ; Find base address
+        SBC     LEN             ; of next record.
+        STA     TBL
+        BCS     DECNUM
+        DEC     TBH
+DECNUM: DEX
+        BPL     RECORD          ; Last record?
+        RTS                     ; z clear.
+        .endproc
+
+; Subroutine HEX. Convert ASCII character pointed to by X to 4 binary bits in A.
+        .proc HEX
+        LDA     IOBUF,X         ; Get character.
+        CMP     #$40            ; Number of letter?
+        BMI     NUMER
+        SEC                     ; Letter; adjust.
+        SBC     #7
+NUMER:  AND     #$0F            ; Convert to binary.
+        RTS
+        .endproc
+
+; Subroutine HX2BIN. Convert 2 ASCII characters on page zero, pointedto by X, to 8 binary bits in X.
+        .proc HX2BIN
+        JSR     HEX             ; Find high byte.
+        ASL     A
+        ASL     A
+        ASL     A
+        ASL     A
+        STA     TEMP
+        INX                     ; and low byte.
+        JSR     HEX
+        ORA     TEMP            ; Combine.
+        TAX
+        RTS
+        .endproc
+
+; Subroutine BIN2HX. Convert 4 bits in A to an ASCII character. Store in page zero, X.
+        .proc   BIN2HX
+        CMP     #$0A            ; Number of letter?
+        BMI     NUMER
+        CLC                     ; Letter; adjust.
+        ADC     #7
+NUMER:  CLC                     ; Convert to ASCII
+        ADC     #'0'
+        STA     IOBUF,X          ; Store character.
+        RTS
+        .endproc
+
+; Subroutine DSPHEX. Convert binary number in A to two ASCII (hexadecimal) characters in page zero locations X, X+1.
+        .proc   DSPHEX
+        PHA                     ; Save number.
+        LSR     A               ; Find high character.
+        LSR     A
+        LSR     A
+        LSR     A
+        JSR     BIN2HX         ; Find low character.
+        INX
+        PLA
+        AND #$0F
+        JSR     BIN2HX
+        RTS
+        .endproc
+
+; Subroutine SYM. Puts base address of symbol table entry X in MISCL, H.
+        .proc SYM
+        SEC                     ; Find difference.
+        STX   TEMP              ; Between last
+        LDA   SYMNUM            ; record and X.
+        SBC   TEMP
+        STA   MISCL
+        LDA   #0
+        STA   MISCH
+        CLC
+        LDY   #2
+X8:     ROL   MISCL             ; Multiply by 8
+        ROL   MISCH             ; bytes per record.
+        DEY
+        BPL   X8
+        SEC                     ; Subtract from
+        LDA   SYMTBL            ; address of
+        SBC   MISCL             ; last record.
+        STA   MISCL
+        LDA   SYMTBH
+        SBC   MISCH
+        STA   MISCH
+        RTS
+        .endproc
+
+; Subroutine ADDRSS. Puts address corresponding to symbol X in ADL. H.
+        .proc ADDRSS
+        JSR   SYM               ; Get base address
+        LDY   #6                ; Get symbol address.
+        LDA   (MISCL),Y
+        STA   ADL               ; Put in ADL, H.
+        INY
+        LDA   (MISCL),Y
+        STA   ADH
+        RTS
+        .endproc
+
+MODLIM: ; Lower opcode pointer limits for modes.
+        .byte $00,$19,$1D,$2A,$3F,$4F,$51,$59,$61,$69,$80,$90,$9C
+
+DECODE:
