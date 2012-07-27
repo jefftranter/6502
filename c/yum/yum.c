10affabdc3f328cea8da9b11a70433117ed5beb6
Jeff Tranter <tranter@pobox.com>
2012-07-27 00:25:10 -0400

    Implementing more code.

--- c/yum/yum.c
+++ c/yum/yum.c
@@ -21,6 +21,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
+ * TO DO:
+ * - compile tome option for all uppercase output
+ * - make computer player smarter
+ *
  * Revision History:
  *
  * Version  Date         Comments
@@ -29,6 +33,8 @@
  *
  */
 
+#include <ctype.h>
+#include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -58,6 +64,8 @@ Press <Enter> to start
 
 Current Score after 6 of 12 turns:
 
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXXXXX       YYYYYYYYXXXXXXXXYYYYYYYY
 Roll            Jeff  Veronica Apple 1
 1's               5      3       4
 2's               -      -       6
@@ -90,15 +98,15 @@ Rolling again...
 Jeff, One your final roll you have:
 3 3 3 6 6
 
-1 - 1's (0/5)
-2 - 2's (0/10)
-3 - 3's (9/15)
-4 - 4's (0/20)
-5 - 5's (0/25)
-6 - 6's (12/30)
-7 - Low Straight (0/15)
-8 - High Straight (0/20)
-9 - Low Score (25)
+1  - 1's (0/5)
+2  - 2's (0/10)
+3  - 3's (9/15)
+4  - 4's (0/20)
+5  - 5's (0/25)
+6  - 6's (12/30)
+7  - Low Straight (0/15)
+8  - High Straight (0/20)
+9  - Low Score (25)
 10 - High Score (25)
 11 - Full House (25/25)
 12 - YUM (0/30)
@@ -134,7 +142,7 @@ Same players as last time (Y/N)? Y
 #define MAXPLAYERS 3
 
 /* Number of categories in score. */
-#define MAXCATEGORY 12
+#define MAXCATEGORY 15
 
 /* Number of dice. */
 #define MAXDICE 5
@@ -144,19 +152,37 @@ Same players as last time (Y/N)? Y
 /* DATA */
 
 /* Number of players. */
-int numPlayers;
+int numHumanPlayers;
+
+/* Number of players. */
+int numComputerPlayers;
+
+/* Current round. */
+int currentRound;
+
+/* Current player. */
+int currentPlayer;
 
 /* Returns true if a given player is a computer. */
 bool isComputerPlayer[MAXPLAYERS];
 
 /* Names of each player. */
-char *playerName[MAXPLAYERS];
+char playerName[MAXPLAYERS][10];
 
 /* 2d array of values given player number and category. */
 int scoreSheet[MAXPLAYERS][MAXCATEGORY];
 
 /* Current dice for player. */
 int dice[MAXPLAYERS][MAXDICE];
+
+/* General purpose buffer for user input. */
+char buffer[40];
+
+/* Names of computer players. */
+char *computerNames[MAXPLAYERS] = { "Apple", "Replica", "Woz" };
+
+/* Names of categories */
+char *labels[MAXCATEGORY] = { "1's", "2's", "3's", "4's", "5's", "6's", "Sub-total", "Bonus", "Low Straight", "High Straight", "Low Score", "High Score", "Full House", "YUM", "TOTAL" };
 
 /* Functions*/
 
@@ -165,7 +191,6 @@ int dice[MAXPLAYERS][MAXDICE];
  * clear player names since we may be starting a new game with the
  * same players.
  */
-
 void initialize()
 {
     int p, c;
@@ -177,24 +202,146 @@ void initialize()
     }
 }
 
-/* Display the current score card. Displays final results if all categories are played. */
-void displayScore();
+/* Print a score value as a number. If set to UNSET, display blanks. */
+void printField(int i)
+{
+    if (i == UNSET) {
+        printf("        ");
+    } else {
+        printf("%-8d", i);
+    }
+}
 
-/* Display help information. */
-void displayHelp();
+/* Print numeric row of the score card. */
+void printRow(char *label, int row)
+{
+    int p;
+
+    printf("%-15s", label);
+    for (p = 0; p < numHumanPlayers + numComputerPlayers; p++) {
+        printField(scoreSheet[p][row]);
+    }
+    printf("\n");
+}
+
+/* Update scores after a turn, i.e. recalculate totals. */
+void updateScore();
+
+/* Display the current score card. Displays final results if all categories are played. */
+void displayScore()
+{
+    int i;
+
+    printf("Current Score after %d of 12 rounds:\n", currentRound);
+    printf("Roll           ");
+    for (i = 0; i < numHumanPlayers + numComputerPlayers; i++) {
+        printf("%-8s", playerName[i]);
+    }
+    printf("\n");
+
+    for (i = 0; i < MAXCATEGORY; i++) {
+        printRow(labels[i], i);
+    }
+}
+
+/* Display help information. Needs to fit on 40 char by 22 line screen. */
+void displayHelp()
+{
+    printf("%s",
+           "This is a computer version of the game\n"
+           "YUM, similar to games known as Yahtzee,\n"
+           "Yacht and Generala. Each player rolls\n"
+           "five dice up to three times and then\n"
+           "applies the dice toward a category to\n"
+           "claim points. The game has 12 rounds\n"
+           "during which each player attempts to\n"
+           "claim the most points in each category.\n"
+           "\n"
+           "The winner is the person scoring the\n"
+           "most points at the end of the game.\n"
+           "\n"
+           "This version supports up to three\n"
+           "players of which any can be human or\n"
+           "computer players.\n");
+}
+
+/* Display a string and prompt for a string. */
+char *promptString(char *string)
+{
+    printf("%s?", string);
+    fgets(buffer, sizeof(buffer)-1, stdin);
+    buffer[strlen(buffer)-1] = '\0'; // Remove newline at end of string
+    return buffer;
+}
+
+/* Display a string and prompt for a number. Number must be in range
+ * min through max. Returns numeric value. TODO: Filter and ignore
+ * invalid characters?
+ */
+int promptNumber(char *string, int min, int max)
+{
+    int val = 0;
+    char *endptr = 0;
+
+    while (true) {
+        printf("%s (%d-%d)?", string, min, max);
+        fgets(buffer, sizeof(buffer)-1, stdin);
+        errno = 0;
+        val = strtol(buffer, &endptr, 10);
+        if ((errno == 0) && (endptr != buffer) && (val >= min) && (val <= max))
+            return val;
+    }
+}
 
 /*
  * Ask number and names of players. Sets values of numPlayers,
  *  isComputerPlayer, and playerName.
  */
+void setPlayers()
+{
+    int i;
 
-void setPlayers();
+    numHumanPlayers = promptNumber("How many human players", 0, MAXPLAYERS);
+    if (numHumanPlayers < MAXPLAYERS) {
+        numComputerPlayers = promptNumber("How many computer players", 0, MAXPLAYERS - numHumanPlayers);
+    } else {
+        numComputerPlayers = 0;
+    }
+
+    for (i = 0; i < numHumanPlayers; i++) {
+        sprintf(buffer, "Name of player %d", i+1);
+        strcpy(playerName[i], promptString(buffer));
+        isComputerPlayer[i] = false;
+    }
+
+    for (i = numHumanPlayers; i < numHumanPlayers + numComputerPlayers; i++) {
+        strcpy(playerName[i], computerNames[i - numHumanPlayers]);
+        isComputerPlayer[i] = true;
+    }
+}
+
+/* Clear the screen */
+void clearScreen()
+{
+    int i;
+    for (i = 0; i < 24; ++i)
+        printf("\n");
+}
 
 /* Display a string and prompt for Y or N. Return boolean value.
-TODO: Remove  need to hit <Enter>?
-*/
-
-bool promptYesNo(char *string);
+ * TODO: Remove need to hit <Enter> ?
+ */
+bool promptYesNo(char *string)
+{
+    while (true) {
+        printf("%s (Y/N)?", string);
+        fgets(buffer, sizeof(buffer)-1, stdin);    
+        if (toupper(buffer[0]) == 'Y')
+            return true;
+        if (toupper(buffer[0]) == 'N')
+            return false;
+    }
+}
 
 /* Display what dice user has (sorted) */
 void displayDice(int playerNum);
@@ -232,24 +379,61 @@ void askComputerDiceToKeep(int playerNum);
 /* Computer decides what category to play. */
 int computerPlayCategory(int playerNum);
 
-/* Call srand() with a key based on player's names to try to make it somewhat random. */
-void setRandomSeed();
+/*
+ * Call srand() with a key based on player's names to try to make it
+ * somewhat random. Be sure to call this after setting player
+ * names. 
+ */
+void setRandomSeed()
+{
+    int i, p, seed;
+
+    seed = 0;
+
+    for (p = 0; p < numHumanPlayers + numComputerPlayers; p++) {
+        for (i = 0; i < strlen(playerName[p]); i++) {
+            seed = (seed << 1) ^ playerName[p][i];
+        }
+    }
+    srand(seed);
+}
 
 /* Main program */
 int main(void)
 {
-    int i, n;
-
-
-    printf("%s", "\nWelcome to YUM!\n");
+    currentRound = 1;
+    currentPlayer = 0;
 
     initialize();
 
-    for (i=0; i < 5; i++) {
-        n = rand() % 6 + 1;
-        printf("%d ", n);
+    clearScreen();
+    printf("%s", "\nWelcome to YUM!\n");
+
+    if (promptYesNo("Do you want instructions")) {
+        clearScreen();
+        displayHelp();        
     }
-    printf("\n");
+
+    setPlayers();
+    printf("Number of human players: %d\n", numHumanPlayers);
+    printf("Number of computer players: %d\n", numComputerPlayers);
+
+    /* This needs to be done after setting player names since the seed is based on names. */
+    setRandomSeed();
+
+    printf("Press <Enter> to start");
+    fgets(buffer, sizeof(buffer)-1, stdin);
+
+    {
+        int i, j;
+        for (i = 0; i < numHumanPlayers + numComputerPlayers; i++) {
+            for (j = 0; j < MAXCATEGORY; j++) {
+                scoreSheet[i][j] = randomNumber(1, 120);
+            }
+        }
+    }
+
+    displayScore();
 
     return 0;
 }
