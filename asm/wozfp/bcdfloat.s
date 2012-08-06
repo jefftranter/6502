64bb5d58b69bacbdf7af282898acfa4603de75cc
Jeff Tranter <tranter@pobox.com>
2012-08-06 18:24:06 -0400

    Enter source code from second COMPUTE article.

--- asm/wozfp/bcdfloat.s
+++ asm/wozfp/bcdfloat.s
@@ -20,12 +20,172 @@
         TEMP   = $0B           ; temporary storage location.
         EVAL   = $0C           ; value of the decimal exponent entered after the "E."
         DEXP   = $17           ; current value of the decimal exponent.
+        BCDA   = $20           ; BCD accumulator (6 bytes)
+        BCDN   = $25           ; ???
+
+; Listing 3. A Floating-Point Binary to BCD Routine.			
+
+        .org $0B00
+BEGIN:  LDA MSB         ; Test MSB to see if mantissa is zero.
+        BNE BRT         ; If it is, print a zero and then get out. Clear display.
+        JSR CLDISP
+        LDA #$30        ; Get ASCII zero.
+        JSR OUTCH       ; Jump to output subroutine.
+        LDA #$0D        ; Get "carriage return."
+        JSR OUTCH       ; Output it.
+        RTS             ; Return to calling routine.
+BRT:    LDA #$00        ; Clear OVFLO location.
+        STA OVFLO
+BRY:    LDA BEXP        ; Is the binary exponent negative?
+        BPL BRZ         ; No.
+        JSR TENX        ; Yes. Multiply by ten until the exponent is not negative.
+        JSR NORM
+        DEC DEXP        ; Decrement decimal exponent.
+        CLV             ; Force a jump.
+        BVC BRY         ; Repeat.	
+BRZ:    LDA BEXP        ; Compare the binary exponent to
+        CMP #$20        ; $20 = 32.
+        BEQ BCD         ; Equal. Convert binary to BCD.
+        BCC BRX1        ; Less than.
+        JSR DIVTEN      ; Greater than. Divide by ten until BEXP is less than 32.
+        INC DEXP
+        CLV             ; Force a jump.
+        BVC BRZ
+BRX1:   LDA #$00        ; Clear OVFLO
+        STA OVFLO
+BRW:    JSR TENX        ; Multiply by ten.
+        JSR NORM        ; Then normalize.
+        DEC DEXP        ; Decrement decimal exponent.
+        LDA BEXP        ; Test binary exponent.
+        CMP #$20        ; Is it 32?
+        BEQ BCD         ; Yes.
+        BCC BRW         ; It's less than 32 so multiply by 10.
+        JSR DIVTEN      ; It's greater than 32 so divide.
+        INC DEXP        ; Increment decimal exponent.
+BRU:    LDA BEXP        ; Test binary exponent.
+        CMP #$20        ; Compare with 32.
+        BEQ BRV         ; Shift mantissa right until exponent
+        LSR MSB         ; is 32.
+        ROR NMSB
+        ROR NLSB
+        ROR LSB
+        ROR TEMP        ; Least-significant bit into TEMP.
+        INC BEXP        ; Increment exponent for each shift
+        CLV             ; right.
+        BVC BRU
+BRV:    LDA TEMP        ; Test to see if we need to round
+        BPL BCD         ; up. No.
+        SEC             ; Yes. Add one to mantissa.
+        LDX #$04
+BRS:    LDA ACC,X
+        ADC #$00
+        STA ACC,X
+        DEX
+        BNE BRS
+BCD:    JSR CONVD       ; Jump to 32 bit binary-to-BCD routine.
+BRMA:   LDY #$04        ; Rotate BCD accumulator right until non-significant zeros are shifted out or DEXP is zero, whichever comes first.
+BRP:    LDX #$04
+        CLC
+BRQ:    ROR BCDA,X
+        DEX
+        BPL BRQ
+        DEY
+        BNE BRP
+        INC DEXP        ; Increment exponent for each shift right. Get out when DEXP = 0.
+        BEQ BROA
+        LDA BCDA        ; Has a non-zero digit been shifted into the least-significant place?
+        AND #$0F
+        BEQ BRMA        ; No. Shift another digit.
+BROA:   NOP             ; Oops. These NOPs cover an earlier mistake.
+        NOP
+        NOP
+        NOP
+        NOP
+        JSR CLDISP      ; This routine simply clears the AIM 65 20-character display.
+        LDA MFLAG
+        BEQ BRNA        ; If the sign of the number is minus, output a minus sign first.
+        LDA #$2D
+        JSR OUTCH       ; ASCII " - " = $2D. Output character.
+BRNA:   LDA #$0B        ; Set digit counter to eleven.
+        STA TEMP
+BRI:    LDY #$04        ; Rotate BCD accumulator left to output most-significant digits first. But first bypass zeros.
+BRH:    CLC
+        LDX #$FB
+BRG:    ROL BCDN
+        INX
+        BNE BRG
+        ROL OVFLO       ; Rotate digit into OVFLO.
+        DEY
+        BNE BRH
+        DEC TEMP        ; Decrement digit counter.
+        LDA OVFLO       ; Is the rotated digit zero?
+        BEQ BRI         ; Yes. Rotate again.
+BRX:    CLC             ; Convert digit to ASCII and output it.
+        ADC #$30
+        JSR OUTCH
+        LDA #$00        ; Clear OVFLO for next digit.
+        STA OVFLO
+        LDY #$04        ; Output the remaining digits.
+BRL:    CLC
+        LDX $FB
+BRJ:    ROL BCDN,X      ; Rotate a digit at a time into
+        INX             ; OVFLO, then output it. One digit is four bits or one nibble.
+        BNE BRJ
+        ROL OVFLO
+        DEY
+        BNE BRL
+        LDA OVFLO       ; Get digit.
+        DEC TEMP        ; Decrement digit counter.
+        BNE BRX
+        LDA DEXP        ; Is the decimal exponent zero?
+        BEQ ARND1       ; Yes. No need to output exponent.
+        LDA #$2E        ; Get ASCII decimal point.
+        JSR OUTCH       ; Output it.
+        LDA #$45        ; Get ASCII "E".
+        JSR OUTCH
+        LDA DEXP        ; Is the decimal exponent plus?
+        BPL THERE       ; Yes.
+        LDA #$2D        ; No. Output ASCII " - "
+        JSR OUTCH
+        LDA DEXP        ; It's minus, so complement it and add one to form the twos complement.
+        EOR #$FF
+        STA DEXP
+        INC DEXP
+THERE:  LDA #$00        ; Clear OVFLO.
+        STA OVFLO
+        SED             ; Convert exponent to BCD.
+        LDY #$08
+BR1A:   ROL DEXP
+        LDA OVFLO	
+        ADC OVFLO
+        STA OVFLO
+        DEY	
+        BNE BR1A
+        CLD
+        CLC
+        LDA OVFLO       ; Get BCD exponent.
+        AND #$F0        ; $Mask low-order nibble (digit).
+        BEQ BR2A
+        ROR A           ; Rotate nibble to the right.
+        ROR A
+        ROR A
+        ROR A
+        ADC #$30        ; Convert to ASCII.
+        JSR OUTCH       ; Output the most-significant digit.
+BR2A:   LDA OVFLO       ; Get the least-significant digit.
+        AND #$0F        ; Mask the high nibble.
+        CLC
+        ADC #$30        ; Convert to ASCII.
+        JSR OUTCH
+ARND1:  LDA #$0D        ; Get an ASCII carriage return.
+        JSR OUTCH
+        RTS             ; All finished.
 
 ; Listing 1: ASCII to Floating-Point Binary Conversion Program
         .org $0E00
 
 START:  CLD             ; Decimal mode not required
-        LDX #$20         ; Clear all the memory locations used for storage by this routine by loading them with zeros.
+        LDX #$20        ; Clear all the memory locations used for storage by this routine by loading them with zeros.
         LDA #$00
 CLEAR:  STA MEM,X
         DEX
@@ -66,11 +226,12 @@ ADDIG:  LDA #$00
         DEC DEXP        ; If set, decrement the exponent, then get another character.
         BMI PLUS
 NORMIZ: JSR NORM        ; Normalize the mantissa.
-        STY TEMP        ; Save Y. It contained the number of "left shifts" in NORM.
-        LDA #$20
-        SEC             ; The binary exponent is 32 - number of left shifts that NORM took to make the most-significant bit one.
-        SBC TEMP
-        STA BEXP
+        CLC             ; Clear carry for addition.
+        LDA BEXP        ; Get binary exponent.
+        ADC #$20        ; Add $20 = 32 to place binary
+        STA BEXP        ; point properly.
+        NOP
+        NOP
         LDA MSB         ; If the MSB of the accumulator is zero, then the number is zero, and its all over. Otherwise, check if the last character was an "E".
         BEQ FINISH1     ; Original listing branched to FINISH but that is too far to reach.
         LDA CHAR
@@ -124,52 +285,60 @@ TENPRW: LDA DEXP        ; Get decimal exponent.
 FINISH1:
         BEQ FINISH      ; If it is zero, routine is done
         BPL MLTPLY      ; If it is plus, go multiply by ten.
-ONCMOR: LDX #$03        ; It's minus. Divide by ten.
-BACK:   ASL LSB         ; First shift the accumulator
-        ROL NLSB        ; three bits left.
+
+ONCMOR: JSR DIVTEN      ; Jump to divide-by-ten subroutine.
+        CLV             ; Force a jump around the routine.
+        BVC ARND        ; The new subroutine is inserted here. Clear accumulator for use as a register. Do $28 = 40 bit divide. OVFLO will be used as "guard" byte.
+DIVTEN: LDA #$00
+        LDY #$28
+BRA:    ASL OVFLO
+        ROL LSB
+        ROL NLSB        ; Roll one bit at a time into the accumulator which serves to hold the partial dividend.
         ROL NMSB
         ROL MSB
-        ROL OVFLO
-        DEC BEXP        ; Decrease the binary exponent for each left shift.
-        DEX
-        BNE BACK
-        LDY #$20        ; Number of trial divisions of $0A into the accumulator giving a $20 = 32 bit quotient.
-AGAIN:  ASL LSB
-        ROL NLSB
-        ROL NMSB
-        ROL MSB
-        ROL OVFLO
-        DEY
-        BEQ OUT         ; Get out when number of trial divisions reaches $20 = 32.
-        LDA OVFLO
-        SEC             ; Subtract 10 = $0A from partial divident in OVFLO.
-        SBC #$0A
-        BMI AGAIN       ; If result is minus, zero into quotient
-        STA OVFLO       ; Otherwise store result in OVFLO, and set bit to one in quotient.
-        INC LSB
-        CLC
-        BCC AGAIN       ; Try it again.
-OUT:    LDA OVFLO       ; Check once more to see if quotient should be rounded upwards.
+        ROL A           ; Check to see if A is larger than the divisor, $0A = 10.
         CMP #$0A
-        BCC AHEAD       ; No.
-        LDX #$04        ; Yes. Add one to quotient.
-REPET:  LDA ACC,X       ; Get each byte of the accumulator and add the carry from the previous addition.
-        ADC #$00
-        STA ACC,X
-        DEX
-        BNE REPET
-        BCC AHEAD       ; What if carry from accumulator occurred? Get mostsignificant byte and put a 1 in bit seven.
-        LDA MSB
-        ORA #$80
-        STA MSB         ; Result into high byte, and increment the binary exponent.
-        INC BEXP
-AHEAD:  LDA MSB         ; Because of three-bit shift at start of division, a one-bit shift (at most) may be required to normalize the mantissa now.
-        BMI ARND
-        ASL LSB
-        ROL NLSB
+        BCC BRB         ; No. Decrease the bit counter.
+        SEC             ; Yes. Subtract divisor from A.
+        SBC #$0A
+        INC OVFLO       ; Set a bit in the quotient.
+BRB:    DEY             ; Decrease the bit counter.
+        BNE BRA
+BRC:    DEC BEXP        ; Division is finished, now normalize.
+        ASL OVFLO       ; For each shift left, decrease the binary exponent.
+        ROL LSB
+        ROL NLSB        ; Rotate the mantissa left until a one is in the most-significant bit.
         ROL NMSB
         ROL MSB
-        DEC BEXP        ; If so, also decrement binary exponent.
+        BPL BRC
+        LDA OVFLO       ; If the most-significant bit in the guard byte is one, round up.
+        BPL BRE
+        SEC             ; Add one.
+        LDX #$04        ; X is byte counter.
+BRD:    LDA ACC,X       ; Get the LSB.
+        ADC #$00        ; Add the carry.
+        STA ACC,X       ; Result into mantissa.
+        DEX
+        BNE BRD         ; Back to complete addition.
+        BCC BRE         ; No carry from MSB so finish.
+        ROR MSB         ; A carry, put in bit seven, and increase the binary exponent.
+        INC BEXP
+BRE:    LDA #$00        ; Clear the OVFLO position, then get out.
+        STA OVFLO
+        RTS
+        NOP             ; Empty memory locations here.
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
 ARND:   LDA #$00        ; Clear overflow byte.
         STA OVFLO
         INC DEXP        ; For each divide-by-10, increment the decimal exponent until it is zero. Then its all over.
@@ -234,18 +403,45 @@ BR8:    LDA ACC,X
         DEX
         BPL BR8
         BMI BR6         ; Check overflow byte once more.
-BR7:    LDY #$00        ; Y will count number of left shifts.
-BR10:   LDA MSB         ; Does most-significant byte have a one in bit seven? Yes, get out.
-        BMI BR11
-        CLC             ; No. Then shift the accumulator left one bit.
+
+BR7:    LDY #$20        ; Y will limit the number of left shifts to 32.
+BR10:   LDA MSB
+        BMI BR11        ; If mantissa has a one in its most-significant bit, get out.
+        CLC
         LDX #$04
-BR9:    ROL ACC,X
+BR9:    ROL ACC,X       ; Shift accumulator left one bit.
         DEX
         BNE BR9
-        INY             ; Keep track of left shifts.
-        CPY #$20        ; Not more than $20 = 32 bits.
-        BCC BR10
+        DEC BEXP        ; Decrement binary exponent for each left shift.
+        DEY
+        BNE BR10        ; No more than $20 = 32 bits shifted.
 BR11:   RTS             ; That's it.
+
+
+; Listing 5. A 32 Bit Binary-to-BCD Subroutine.
+
+        .org $0D67
+CONVD:  LDX  #$05       ; Clear BCD accumulator.
+        LDA #$00
+BRM:    STA BCDA,X      ; Zeros into BCD accumulator.
+        DEX
+        BPL BRM
+        SED             ; Decimal mode for add.
+        LDY #$20        ; Y has number of bits to be converted. Rotate binary number into carry.
+BRN:    ASL LSB
+        ROL NLSB
+        ROL NMSB
+        ROL MSB
+        LDX #$FB        ; X will control a five byte addition. Get least-significant byte of the BCD accumulator, add is to itself, then store.
+BRO:    LDA BCDA,X
+        ADC BCDA,X
+        STA BCDA,X
+        INX             ; Repeat until all five bytes have been added.
+        BNE BRO
+        DEY             ; Get another bit from the binary number.
+        BNE BRN
+        CLD             ; Back to binary mode.
+        RTS             ; And back to the program.
 
 ; Listing 4. AIM 65 Input/Output Subroutines.
 
@@ -297,3 +493,14 @@ CLDISP: LDX #$13
         DEX
         BPL $0F9F
         RTS
+
+; Listing 4. Subroutine OUTCH For the AIM 65.
+
+          PRINT = $F000
+          MODIFY = $0F72
+          DISPLAY = $0F60
+
+OUTCH:    JSR PRINT     ; AIM 65 monitor subroutine.
+          JSR MODIFY    ; See previous article in COMPUTE!
+          JSR DISPLAY   ; See previous article in COMPUTE!
+          RTS
