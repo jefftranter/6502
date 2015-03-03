c59d8a1c912572209e1cf1c35e7bf2450ece63c1
Jeff Tranter <tranter@pobox.com>
2015-03-02 23:09:10 -0500

    Add Bandit program.

---
+++ asm/KIM-1/TheFirstBookOfKIM/Bandit/README.txt
@@ -0,0 +1,34 @@
+BANDIT
+JIM BUTTERFIELD
+
+Start the program at 0200 and on the right, you'll see the $25
+that KIM has given you to play with. The funny symbols on the
+left are your "wheels" - hit any key and see them spin.
+
+Every time you spin the wheels by hitting a key it costs you $1.
+When the wheels stop, you might have a winning combination,
+in which case you'll see money being added to your total
+on the right. Most of the time, you'll get nothing ...
+but that's the luck of the game.
+
+The biggest jackpot is $15: that's three bars across the
+display. Other combinations pay off, too; you'll soon learn
+to recognize the "cherry" symbol, which pays $2 every time
+it shows in the left hand window.
+
+There's no house percentage, so you can go a long time on
+your beginning $25. The most you can make is $99; and if
+you run out of money, too bad: KIM doesn't give credit.
+
+You'll notice that the listing for BANDIT looks a little
+different from others in this book. That's because it
+is the output of a resident assembler operating in
+an expanded KIM system. See the section on expansion
+for a further discussion of assemblers.
+
+You might like to change the payouts so that there is
+a "house percentage". That way, visitors will eventually
+run out of money if they play long enough. This has
+two possible advantages: it will teach them the evils
+of gambling, and they won't hog your KIM all day playing
+this game.
---
+++ asm/KIM-1/TheFirstBookOfKIM/Bandit/bandit.s
@@ -0,0 +1,151 @@
+; ******************************
+; ***** ONE ARMED BANDIT   *****
+; ***** BY JIM BUTTERFIELD *****
+; ******************************
+
+WINDOW  = $0000                 ; DISPLAY WINDOW
+AMT     = $0005                 ; CASH CACHE
+ARROW   = $0006
+RWD     = $0007                 ; REWARD
+STALLA  = $0008                 ; WAIT WHILE
+TUMBLE  = $0009
+
+; LINKAGES TO KIM
+
+KEYIN  = $1F40                  ; IS KEY DEPRESSED?
+PADD   = $1741
+SAD    = $1740
+SBD    = $1742
+TABLE  = $1FE7                  ; HEX:7 SEG
+
+; MAIN PROGRAM
+
+       .ORG $0200
+
+GO:    LDA     #$25             ; GIVE HIM $25
+       STA     AMT              ; TO START WITH
+       JSR     CVAMT            ; AND SHOW IT TO HIM.
+       LDA     #$00             ; RESET ARROW.
+       STA     ARROW
+
+; MAIN DISPLAY LOOP
+
+LPA:   JSR     DISPLY           ; DISPLAY UNTIL
+       BNE     LPA              ; [GO] IS RELEASED
+ROLL:  INC     TUMBLE           ; RANDOMIZE TUMBLE
+       JSR     DISPLY           ; DISPLAY UNTIL
+       BEQ     ROLL             ; A KEY IS HIT
+
+       LDA     #$03
+       STA     ARROW
+       SED
+       SEC
+       LDA     AMT
+       SBC     #$01            ; CHARGE ONE BUCK.
+       STA     AMT
+       JSR     CVAMT           ; CONVERT FOR LED
+       ROL     TUMBLE
+
+LPB:   JSR     DISPLY
+       DEC     STALLA          ; DISPLAY A WHILE.
+       BNE     LPB
+       LDX     ARROW
+       LDA     TUMBLE          ; MAKE A
+       AND     #$06            ; RESULT
+       ORA     #$40
+
+       STA     WINDOW+1,X
+       LSR     TUMBLE
+       LSR     TUMBLE          ; DO ALL
+       DEC     ARROW           ; 3 WINDOWS
+       BNE     LPB
+
+; ALL WHEELS STOPPED - COMPUTE PAYOFF
+
+       LDA     WINDOW+4
+       CMP     WINDOW+3        ; CHECK FOR
+       BNE     NOMAT           ; A MATCH.
+       CMP     WINDOW+2
+       BNE     NOMAT
+       LDX     #$10
+       CMP     #$40            ; PAY $15 FOR 3 BARS
+       BEQ     PAY
+       LDX     #$08
+       CMP     #$42            ; PAY $10 FOR 3 UPS
+       BEQ     PAY
+       LDX     #$06
+       CMP     #$44            ; PAY $5 FOR 3 DOWNS
+       BEQ     PAY
+       DEX
+
+; A WIN!!! PAY AMOUNT IN X
+
+PAY:   STX     RWD             ; HIDE REWARD
+PAX:   LDA     #$80
+       STA     STALLA
+LPC:   JSR     DISPLY          ; DISPLAY
+       DEC     STALLA          ; FOR A HALF
+       BNE     LPC             ; A WHILE.
+       DEC     RWD
+       BEQ     LPA
+       CLC                     ; SLOWLY ADD
+       SED                     ; THE PAYOFF
+       LDA     AMT             ; TO THE AM'T.
+       ADC     #$01
+       BCS     LPA
+       STA     AMT
+       JSR     CVAMT
+       BNE     PAX
+
+; WHEELS NOT ALL THE SAME - CHECK FOR SMALL PAYOFF
+
+NOMAT: LDX     #$03
+       CMP     #$46            ; A CHERRY?
+       BEQ     PAY
+LOK:   JSR     DISPLY
+       LDA     AMT             ; CAN'T PLAY
+       BNE     LPA             ; WITH NO DOUGH!
+       BEQ     LOK
+
+; DISPLAY SUBROUTINE
+
+DISPLY: LDX    ARROW
+       BPL     INDIS           ; ROLL
+OVER:  INC     WINDOW+2,X      ; THE DRUM
+INDIS: DEX
+       BPL     OVER
+       LDA     #$7F
+       STA     PADD
+       LDY     #$08
+       LDX     #$04
+LITE:  LDA     WINDOW,X        ; LIGHT
+       STY     SBD             ; ALL THE
+       STA     SAD             ; WINDOWS
+       CLD
+       LDA     #$7F
+ZIP:   SBC     #$01
+       BNE     ZIP
+       STA     SBD
+       INY
+       INY
+       DEX
+       BPL     LITE
+       JSR     KEYIN
+       RTS
+
+; AMOUNT CONVERSION
+
+CVAMT: LDA     AMT
+       AND     #$0F            ; TRANSLATE
+       TAX                     ; AMOUNT
+       LDA     TABLE,X         ; TO LED
+       STA     WINDOW          ; CODE.
+       LDA     AMT
+       LSR     A
+       LSR     A
+       LSR     A
+       LSR     A
+       TAX
+       LDA     TABLE,X
+       STA     WINDOW+01
+       RTS
