a26042202bcc6f07db061e70c87c8afa4b5a4bfd
Jeff Tranter <tranter@pobox.com>
2015-03-03 17:59:33 -0500

    Add Bitz.

---
+++ asm/KIM-1/TheFirstBookOfKIM/Bitz/README.txt
@@ -0,0 +1,22 @@
+Bitz
+BY JIM BUTTERFIELD
+
+A teaching program which drills you on binary and
+hexadecimal numbering schemes.  It's kind of fun
+just as a speed test.
+
+Start the program at 0200 and you'll see eight bits
+on the left side of the display.  Some of the bits
+are in the lower position, meaning 'off' or zero.
+Others will be in the top row, where they mean 'on'
+or logic one.
+
+All you have to do is translate those bits into hexadecimal
+notation, and enter the hex value.  For example, if all
+bits happen to be 'on', the number you'd enter is FF;
+or if all the bits were 'off', you'd enter 00.
+KIM rewards a correct answer with another problem.
+
+If you're not yet at ease with the concept of bits
+and how they relate to hexadecimal numbering, a few
+runs of this program will help a lot.
---
+++ asm/KIM-1/TheFirstBookOfKIM/Bitz/bitz.s
@@ -0,0 +1,85 @@
+        .org    $0200
+
+MOD     = $11
+PREV    = $14
+SEED    = $15
+SEED1   = $16
+SEED2   = $17
+FLAG1   = $1C
+FLAG2   = $1D
+SAD     = $1740
+SADD    = $1741
+SBD     = $1742
+TIMER   = $1AF4
+TABLE   = $1FE7
+KEYIN   = $1F40
+GETKEY  = $1F6A
+
+START:  CLD
+        LDA     #1              ; Set FLAG2
+        STA   FLAG2             ; ..to new problem
+MAIN:   JSR   KEYIN             ; set directnl reg
+        JSR   GETKEY            ; get key input
+        CMP   PREV              ; same as last time?
+        BEQ   LIGHT             ; yes, skip
+        STA   PREV              ; record new input
+        CMP   #$15              ; no key?
+        BEQ   NOKEY             ; yes, brnch
+        LDX   FLAG1             ; first digit found?
+        BNE   DIG1              ; yes, check second
+        CMP   SEED1             ; first digit match?
+        BNE   LIGHT             ; no, ignore input
+        TAX
+        LDA   TABLE,X           ; change to segment
+        STA   FLAG1             ; ..store.
+        BNE   LIGHT             ; ..and exit
+DIG1:   CMP   SEED2             ; second digit match?
+        BNE   LIGHT             ; no, ignore input
+        TAX
+        LDA   TABLE,X           ; change to segment
+        STA   FLAG2
+        BNE   LIGHT
+NOKEY:  LDX   FLAG2             ; problem solved?
+        BEQ   LIGHT             ; not yet, skip
+        LDA   #0                ; Clear..
+        STA   FLAG1             ; ..for new problem
+        STA   FLAG2
+        LDA   TIMER             ; get random value
+        TAX
+        AND   #$0F              ; extract last digit
+        STA   SEED2             ; ..and store
+        TXA
+        LSR   A                 ; Extract first digit
+        LSR   A
+        LSR   A
+        LSR   A
+        STA   SEED1             ; ..and store
+        STX   SEED              ; Store whole number
+        LDX   #$FC              ; Minus 4 for window
+PATT:   LDA   #0                ; Clear Accum
+        ROL   SEED              ; ..then roll in..
+        ROL   A                 ; ..two bits..
+        ROL   SEED              ; ..and..
+        ROL   A                 ; ..convert..
+        TAY                     ; ..to..
+        LDA   TAB,Y             ; ..segments
+        STA   FLAG1,X
+        INX                     ; next segment
+        BNE   PATT
+LIGHT:  LDA   #$7F              ; Set directional..
+        STA   SADD              ; ..registers
+        LDY   #9
+        LDX   #$FA              ; Minus 6
+SHOW:   LDA   FLAG2+1,X         ; Window contents
+        STA   SAD
+        STY   SBD
+WAIT:   DEC   MOD
+        BNE   WAIT
+        INY
+        INY
+        INX
+        BMI   SHOW
+        BPL   MAIN
+TAB:    .BYTE $14,$12,$24,$22
+
+; end
