17f88353656279626d26dea7a23c8fd4eba5801b
Jeff Tranter <tranter@pobox.com>
2015-03-05 21:47:26 -0500

    Add another program.

---
+++ asm/KIM-1/TheFirstBookOfKIM/Farmer Brown/README.txt
@@ -0,0 +1,54 @@
+FARMER BROWN
+by Jim Butterfield
+
+You are farmer Brown. You are growing a beautiful crop of corn
+But the following animals try to come and steal your corn:
+
+Ant     Bird     Cow     Dog     Elephant     Fox
+
+As soon as you see one of these animals coming for your corn,
+you can scare it away by calling its name. Press the button
+with the first letter of the animal's name. So you would
+press A to shoo away an ant, B to shoo away a bird, and so on.
+
+If you press the right button, the animal will go back. If you
+press the wrong button, it will think you mean somebody else
+and keep coming for your corn. And when all your corn is gone,
+KIM will show 000 and the game is over.
+
+The animal won't "shoo" unless it has completely entered the
+display. Speed of the animals can be adjusted by changing the
+contents of location 026A.
+
+-----------------------------------------------------
+
+FARMER BROWN....
+
+Exercises:
+
+1. You can see that each animal occupies 6 memory locations,
+starting at 02AA (the Ant) - and the last location must always
+be zero. Can you make up your own animals? The letters may
+not fit exactly, but you can always invent names or use
+odd ones (you could make an Aardvark, a Burfle, a Cobra, and
+so on).
+
+2. The game might be more fun if the animals went faster after
+a while, so that sooner or later they would just zip by.
+The location that controls speed is at address 026A;
+the lower the number, the faster the animals will go.
+So if you could arrange to have the program decrease
+this number automatically once in a while, you'd get
+a nice speed-up feature.
+
+3. You can't "shoo" the animal until it's completely entered
+the display; but you can still catch it after it's partly
+left. The game would be harder - and maybe more fun -
+if you could only shoo it while it was completely in the
+display. Hint - testing location 00$F (WINDOW-1) would
+tell you if an animal was on its way out.
+
+4. You'd have a "Target Practice" game if you made the animal
+disappear (instead of backing up) when you pressed the
+right button. With a little planning, you'll find that
+this is quite easy to do.
---
+++ asm/KIM-1/TheFirstBookOfKIM/Farmer Brown/farmerbrown.s
@@ -0,0 +1,112 @@
+        WINDOW  = $60
+        WINGS   = $66
+        FLAG    = $6C
+        GOT     = $6D
+        CORN    = $6E
+        KEY     = $6F
+        POINL   = $70
+        POINH   = $71
+        DELAY   = $72
+        WAIT    = $73
+
+        TIMER   = $1704
+        SAD     = $1740
+        PADD    = $1741
+        SBD     = $1742
+        DONE    = $1925
+        KEYIN   = $1F40
+        GETKEY  = $1F6A
+
+        .ORG    $0200
+
+START:  LDX     #13
+        STX     CORN            ; bushels of corn to start
+        LDA     #0              ; clear the window
+SLOOP:  STA     WINDOW,X
+        DEX
+        BPL     SLOOP
+TEST:   LDX     #11             ; is window enipty?
+TLOOP:  LDA     WINDOW,X
+        BNE     CONTIN          ; no. keep going
+        DEX
+        BPL     TLOOP
+        INC     GOT             ; yes. make new animal
+        LDA     FLAG
+        BEQ     MORE            ; did last animal get in?
+        DEC     GOT
+        DEC     CORN            ; take away some corn
+        BNE     MORE            ; any left?
+        JMP     DONE            ; no, end of game
+MORE:   LDA     TIMER           ; random value..
+        LSR     A               ; ..to generate..
+        LSR     A
+        LSR     A
+        LSR     A               ; ..new random animal
+        LSR     A
+        CMP     #6              ; 6 types of animal
+        BCC     MAKE
+        AND     #$03
+MAKE:   CLC
+        TAX                     ; animal type to X
+        ADC     #$0A            ; key type A to F
+        STA     KEY
+        LDA     INDEX,X         ; animal 'picture' address
+        STA     POINL           ; to indirect pointer
+        LDA     #2
+        STA     POINH
+        LDY     #5              ; six locations to move
+ALOOP:  LDA     (POINL),Y       ; from 'picture'
+        STA     WINGS,Y         ; ..to 'wings'
+        DEY
+        BPL     ALOOP
+        STY     FLAG            ; flag FF - animal coming
+CONTIN: LDX     #5              ; test:
+CLOOP:  LDA     WINGS,X         ; is animal out of 'wings'?
+        BNE     NOKEY           ; no, ignore keyboard
+        DEX
+        BPL     CLOOP
+        JSR     KEYIN
+        JSR     GETKEY
+        CMP     KEY             ; right animal named?
+        BNE     NOKEY           ; no, ignore key
+        LDA     FLAG
+        BPL     NOKEY           ; animal retreating?
+        INC     FLAG            ; make animal retreat
+NOKEY:  DEC     DELAY           ; wait a while..
+        BNE     NOMOVE          ; before moving animal
+        LDA     #$20            ; speed control value
+        STA     DELAY
+        LDA     FLAG            ; move animal - which way?
+        BMI     COMING          ; ..left
+        LDX     #10             ; ..right
+RLOOP:  LDA     WINDOW-6,X
+        STA     WINDOW-5,X
+        DEX
+        BNE     RLOOP
+        STX     WINDOW-6        ; clear extreme left
+        BEQ     NOMOVE          ; unconditional branch
+COMING: LDX     #$F0            ; -16
+CMLOOP: LDA     WINDOW+12,X
+        STA     WINDOW+11,X
+        INX
+        BMI     CMLOOP
+NOMOVE: LDA     #$7F            ; light KIM display
+        STA     PADD
+        LDY     #$13
+        LDX     #5              ; six display digits
+LITE:   LDA     WINDOW,X
+        STA     SAD
+        STY     SBD
+LITEX:  INC     WAIT
+        BNE     LITEX
+        DEY
+        DEY
+        DEX
+        BPL     LITE
+        JMP     TEST
+
+; index and animal 'pictures' in hexadecimal form
+
+INDEX:  .BYTE $AA, $B0, $B6, $BC, $C2, $C8, $08, $00, $00, $00, $00, $00, $01, $61, $61, $40, $00, $00
+        .BYTE $61, $51, $47, $01, $00, $00, $63, $58, $4E, $00, $00, $00, $71, $1D, $41, $1F, $01, $00
+        .BYTE $63, $58, $4C, $40, $00, $00
