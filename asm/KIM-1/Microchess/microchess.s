28283017356e1efb091577ca0fa5a3eb891758e6
Jeff Tranter <tranter@pobox.com>
2015-03-15 16:09:36 -0400

    Almost assembling now.

--- asm/KIM-1/Microchess/microchess.s
+++ asm/KIM-1/Microchess/microchess.s
@@ -26,7 +26,7 @@
         PCAP    = $E6
         BMOB    = $E3
         BMAXC   = $E4
-;        BCC     = $E5
+        BBCC    = $E5
         BMAXP   = $E6
         XMAXC   = $E8
         WMOB    = $EB
@@ -468,221 +468,203 @@ UMOVE:   TSX                    ; UNMAKE MOVE
          PLA                    ; CAPTURED
          STA     PIECE          ; PIECE
          TAX
-401  033E 68                     PLA                      FROM SQUARE
-402  033F 95 50                  STAX     .BOARD
-403  0341 68                     PLA                      PIECE
-404  0342 AA                     TAX
-405  0343 68                     PLA                      TO SOUARE
-406  0344 85 B1                  STA      .SQUARE
-407  0346 95 50                  STAX     .BOARD
-408  0348 4C 70 03               JMP       STRV
-409                    ;
-410                    ;       THIS ROUTINE MOVES .PIECE
-411                    ;       TO .SQUARE,  PARAMETERS
-412                    ;       ARE SAVED IN A STACK TO UNMAKE
-413                    ;       THE MOVE LATER
-414                    ;
-415  034B BA           MOVE      TSX
-416  034C 86 B3                  STXZ      .SPI           SWITCH
-417  034E A6 B2                  LDX      .SP2           STACKS
-418  0350 9A                     TXS
-419  0351 A5 B1                  LDA      .SQUARE
-420  0353 48                     PHA                      TO SQUARE
-421  0354 A8                     TAY
-422  0355 A2 1F                  LDX #     1F
-423  0357 D5 50        CHECK     CMPZX     .BOARD         CHECK FOR
-424  0359 F0 03                  BEQ       TAKE           CAPTURE
-425  035B CA                     DEX
-426  035C 10 F9                  BPL       CHECK
-427  035E A9 CC        TAKE      LDA #     CC
-428  0360 95 50                  STAX     .BOARD
-429  0362 8A                     TXA                      CAPTURED
-430  0363 48                     PHA                      PIECE
-431  0364 A6 B0                  LDX      .PIECE
-432  0366 B5 50                  LDAX     .BOARD
-433  0368 94 50                  STYZX     .BOARD         FROM
-434  036A 48                     PHA                         SQUARE
-435  036B 8A                     TXA
-436  036C 48                     PHA                      PIECE
-437  036D A5 B6                  LDA      .MOVEN
-438  036F 48                     PHA                      MOVEN
-439  0370 BA           STRV      TSX
-440  0371 86 B2                  STXZ      .SP2           SWITCH
-441  0373 A6 B3                  LDX      .SPI           STACKS
-442  0375 9A                     TXS                      BACK
-443  0376 60                     RTS
-444                    ;
-445                    ;       CONTINUATION OF SUB STRATGY
-446                    ;       -CHECKS FOR CHECK OR CHECKMATE
-447                    ;       AND ASSIGNS VALUE TO MOVE
-448                    ;
-449  0377 A6 E4        CKMATE    LDX      .BMAXC         CAN BLK CAP
-450  0379 E4 A0                  CPXZ      .POINTS        MY KING?
+         PLA                    ; FROM SQUARE
+         STA     BOARD,X
+         PLA                    ; PIECE
+         TAX
+         PLA                    ; TO SOUARE
+         STA     SQUARE
+         STA     BOARD,X
+         JMP     STRV
+;
+;       THIS ROUTINE MOVES .PIECE
+;       TO .SQUARE,  PARAMETERS
+;       ARE SAVED IN A STACK TO UNMAKE
+;       THE MOVE LATER
+;
+MOVE:    TSX
+         STX     SPI            ; SWITCH
+         LDX     SP2            ; STACKS
+         TXS
+         LDA     SQUARE
+         PHA                    ; TO SQUARE
+         TAY
+         LDX     #$1F
+CHECK:   CMP     BOARD,X        ; CHECK FOR
+         BEQ     TAKE           ; CAPTURE
+         DEX
+         BPL     CHECK
+TAKE:    LDA     #$CC
+         STA     BOARD,X
+         TXA                    ; CAPTURED
+         PHA                    ; PIECE
+         LDX     PIECE
+         LDA     BOARD,X
+         STY     BOARD,X        ; FROM
+         PHA                    ; SQUARE
+         TXA
+         PHA                    ; PIECE
+         LDA     MOVEN
+         PHA                    ; MOVEN
+STRV:    TSX
+         STX     SP2            ; SWITCH
+         LDX     SPI            ; STACKS
+         TXS                    ; BACK
+         RTS
+;
+;       CONTINUATION OF SUB STRATGY
+;       -CHECKS FOR CHECK OR CHECKMATE
+;       AND ASSIGNS VALUE TO MOVE
+;
+CKMATE:  LDX     BMAXC          ; CAN BLK CAP
+         CPX     POINTS         ; MY KING?
+         BNE     NOCHEK
+         LDA     #$00           ; GULP!
+         BEQ     RETV           ; DUMB MOVE!
+;
+NOCHEK:  LDX     BMOB           ; IS BLACK
+         BNE     RETV           ; UNABLE TO
+         LDX     WMAXP          ; MOVE AND
+         BNE     RETV           ; KING IN CH?
+         LDA     #$FF           ; YES! MATE
+;
+RETV:    LDX     #$04           ; RESTORE
+         STX     STATE          ; STATE=4
+;
+;       THE VALUE OF THE MOVE (IN ACCU)
+;       IS COMPARED TO THE BEST MOVE AND
+;       REPLACES IT IF IT IS BETTER
+;
+PUSH:    CMP     BESTV          ; IS THIS BEST
+         BCC     RETP           ; MOVE SO FAR?
+         BEQ     RETP
+         STA     BESTV          ; YES!
+         LDA     PIECE          ; SAVE IT
+         STA     BESTP
+         LDA     SQUARE
+         STA     BESTM          ; FLASH DISPLAY
+RETP:    JMP     SCANDS         ; AND RTS
+;
+;       MAIN PROGRAM TO PLAY CHESS
+;       PLAY FROM OPENING OR THINK
+;
+GO:      LDX     OMOVE          ; OPENING?
+         BPL     NOOPEN         ; -NO
+         LDA     DIS3           ; -YES WAS
+         CMP     OPNING,X       ; OPPONENT'S
+         BNE     END            ; MOVE OK?
+         DEX
+         LDA     OPNING,X       ; GET NEXT
+         STA     DIS1           ; CANNED
+         DEX                    ; OPENING MOVE
+         LDA     OPNING,X
+         STA     DIS3           ; DISPLAY IT
+         DEX
+         STX     OMOVE          ; MOVE IT
+         BNE     MV2            ; (JMP)
+;
+END:     STA     OMOVE          ; FLAG OPENING
+NOOPEN:  LDX     #$0C           ; FINISHED
+         STX     STATE          ; STATE=C
+         STX     BESTV          ; CLEAR BESTV
+         LDX     #$14           ; GENERATE P
+         JSR     GNMX           ; MOVES
+;
+         LDX     #$04           ; STATE=4
+         STX     STATE          ; GENERATE AND
+         JSR     GNMZ           ; TEST AVAILABLE
+;                                 MOVES
+;
+         LDX     BFSTV          ; GET BEST MOVE
+         CPX     #$0F           ; IF NONE
+         BCC     MATE           ; OH OH!
+;
+MV2:     LDX     BESTP          ; MOVE
+         LDA     BOARD,X        ; THE
+         STA     BESTV          ; BEST
+         STX     PIECE          ; MOVE
+         LDA     BESTM
+         STA     SQUARE         ; AND DISPLAY
+         JSR     MOVE           ; IT
+         JMP     CHESS
+;
+MATE:    LDA     #$FF           ; RESIGN
+         RTS                    ; OR STALEMATE
+;
+;       SUBROUTINE TO ENTER THE 
+;       PLAYER'S MOVE
+;
+DISMV:   LDX     #$04           ; ROTATE
+ROLL:    ASL     DIS3           ; KEY
+         ROL     DIS2           ;INTO
+         DEX                    ; DISPLAY
+         BNE     ROLL
+         ORA     DIS3
+         STA     DIS3
+         STA     SQUARE
+         RTS
+;
+;       THE FOLLOWING SUBROUTINE ASSIGNS
+;       A VALUE TO THE MOVE UNDER
+;       CONSIDERATION AND RETURNS IT IN
+;         THE ACCUMULATOR
+;
 
-
-CHESS                   PAGE 10
-
-
-
-451  037B D0 04                  BNE NOCHEK
-452  037D A9 00                  LDA #     00             GULP!
-453  037F F0 0A                  BEQ       RETV           DUMB MOVE!
-454                    ;
-455  0381 A6 E3        NOCHEK    LDX      .BMOB          IS BLACK
-456  0383 D0 06                  BNE       RETV           UNABLE TO
-457  0385 A6 EE                  LDX      .WMAXP         MOVE AND
-458  0387 D0 02                  BNE       RETV           KING IN CH?
-459  0389 A9 FF                  LDA #     FF             YES! MATE
-460                    ;
-461  038B A2 04        RETV      LDX #     04             RESTORE
-462  038D 86 B5                  STXZ      .STATE         STATE=4
-463                    ;
-464                    ;       THE VALUE OF THE MOVE (IN ACCU)
-465                    ;       IS COMPARED TO THE BEST MOVE AND
-466                    ;       REPLACES IT IF IT IS BETTER
-467                    ;
-468  038F C5 FA        PUSH      CMPZ      .BESTV         IS THIS BEST
-469  0391 90 0C                  BCC       RETP           MOVE SO FAR?
-470  0393 F0 0A                  BEQ       RETP
-471  0395 85 FA                  STA      .BESTV         YES!
-472  0397 A5 B0                  LDA      .PIECE         SAVE IT
-473  0399 85 FB                  STA      .BESTP
-474  039B A5 B1                  LDA      .SQUARE
-475  039D 85 F9                  STA      .BESTM         FLASH DISPLAY
-476  039F 4C 1F 1F     RETP      JMP       *OUT           AND RTS
-477                    ;
-478                    ;       MAIN PROGRAM TO PLAY CHESS
-479                    ;       PLAY FROM OPENING OR THINK
-480                    ;
-481  03A2 A6 DC        GO        LDX      .OMOVE         OPENING?
-482  03A4 10 17                  BPL       NOOPEN             -NO
-483  03A6 A5 F9                  LDA      .DIS3          -YES WAS
-484  03A8 D5 DC                  CMPZX     .OPNING         OPPONENT'S
-485  03AA D0 0F                  BNE       END             MOVE OK?
-486  03AC CA                     DEX
-487  03AD B5 DC                  LDAX     .OPNING        GET NEXT
-483  03AF 85 FB                  STA      .DIS1          CANNED
-489  03B1 CA                     DEX                      OPENING MOVE
-490  03B2 B5 DC                  LDAX     .OPNING
-491  03B4 85 F9                  STA      .DIS3          DISPLAY IT
-492  03B6 CA                     DEX
-493  03B7 86 DC                  STXZ      .OMOVE         MOVE IT
-494  03B9 D0 1A                  BNE       MV2            (JMP)
-495                    ;
-496  03BB 85 DC        END       STA      .OMOVE         FLAG OPENING
-497  03BD A2 0C        NOOPEN    LDX #     OC             FINISHED
-498  03BF 86 B5                  STXZ      .STATE         STATE=C
-499  03C1 86 FA                  STXZ      .BESTV         CLEAR BESTV
-500  03C3 A2 14                  LDX #     14             GENERATE P
-
-
-CHESS                   PAGE 11
-
-
-
-501  03C5 20 02 02               JSR       GNMX           MOVES
-502                    ;
-503  03C8 A2 04                  LDX #     04             STATE=4
-504  03CA 86 B5                  STXZ      .STATE         GENERATE AND
-505  03CC 20 00 02               JSR       GNMZ           TEST AVAILABLE
-506                    ;                                  MOVES
-507                    ;
-508  03CF A6 FA                  LDX      .BFSTV         GET BEST MOVE
-509  03D1 E0 0F                  CPX #     0F             IF NONE
-510  03D3 90 12                  BCC       MATE           OH OH!
-511                    ;
-512  03D5 A6 FB        MV2       LDX      .BESTP         MOVE
-513  03D7 B5 50                  LDAX     .BOARD          THE
-514  03D9 85 FA                  STA      .BESTV         BEST
-515  03DB 86 B0                  STXZ      .PIECE         MOVE
-516  03DD A5 F9                  LDA      .BESTM
-517  03DF 85 B1                  STA      .SQUARE        AND DISPLAY
-518  03E1 20 4B 03               JSR       MOVE             IT
-519  03E4 4C 00 00               JMP       CHESS
-520                    ;
-521  03E7 A9 FF        MATE      LDA #     FF             RESIGN
-522  03E9 60                     RTS                      OR STALEMATE
-523                    ;
-524                    ;       SUBROUTINE TO ENTER THE 
-525                    ;       PLAYER'S MOVE
-526                    ;
-527  03EA A2 04        DISMV     LDX #     04             ROTATE
-528  03EC 06 F9        ROL       ASLZ      .DIS3           KEY
-529  03EE 26 FA                  ROLZ      .DIS2          INTO
-530  03F0 CA                     DEX                      DISPLAY
-531  03F1 D0 F9                  BNE       ROL
-532  03F3 05 F9                  ORAZ      .DIS3
-533  03F5 85 F9                  STA      .DIS3
-534  03F7 85 B1                  STA      .SQUARE
-535  03F9 60                     RTS
-536                    ;
-537                    ;       THE FOLLOWING SUBROUTINE ASSIGNS
-538                    ;       A VALUE TO THE MOVE UNDER
-539                    ;       CONSIDERATION AND RETURNS IT IN
-540                    ;         THE ACCUMULATOR
-541                    ;
-542                              +++
-543  1780 18           STRATGY   CLC
-544  1781 A9 80                  LDA #      80
-545  1783 65 EB                  ADCZ       .WMOB          PARAMETERS
-546  1785 65 EC                  ADCZ       .WMAXC         WITH WEIGHT
-547  1787 65 ED                  ADCZ       .WCC           OF 0.25
-548  1789 65 E1                  ADCZ       .WCAP1
-549  178B 65 DF                  ADCZ       .WCAP2
-550  178D 38                     SEC
-
-
-CHESS                   PAGE 12
-
-
-
-551  178E B5 F0                  SBCZ       .PMAXC
-552  1790 E5 F1                  SBCZ       .PCC
-553  1792 E5 E2                  SBCZ       .BCAPO
-554  1794 E5 E0                  SBCZ       .BCAP1
-555  1796 E5 DE                  SBCZ       .BCAP2
-556  1798 E8 EF                  SBCZ       .PMOB
-557  179A E5 E3                  SBCZ       .BMOB
-558  179C B0 02                  BCS        POS           UNDERFLOW
-559  179E A9 00                  LDA #      00            PREVENTION
-560  17A0 4A           POS       LSRA
-561  17A1 18                     CLC                      **************
-562  17A2 69 40                  ADCIM      40
-563  17A4 65 EC                  ADCZ       .WMAXC        PARAMETERS
-564  17A6 65 ED                  ADCZ       .WCC          WITH WEIGHT
-565  17A8 38                     SEC                      OF 0.5
-566  17A9 E5 E4                  SBCZ       .BMAXC
-567  17AB 4A                     LSRA                     **************
-568  17AC 18                     CLC
-569  17AD 69 90                  ADCIM      90
-570  17AF 65 DD                  ADCZ       .WCAPO        PARAMETERS
-571  17B1 65 DD                  ADCZ       .WCAPO        WITH WEIGHT
-572  17B3 65 DD                  ADCZ       .WCAP0        OF 1.0
-573  17B5 65 DD                  ADCZ       .WCAP0
-574  17B7 65 E1                  ADCZ       .WCAP1
-575  17B9 38                     SEC                      [UNDER OR OVER-
-576  17BA E5 E4                  SBCZ       .BMAXC         FLOW MAY OCCUR
-577  17BC E5 E4                  SBCZ       .BMAXC         FROM THIS
-578  17BE E5 E5                  SBCZ       .BCC           SECTION]
-579  17C0 E5 E5                  SBCZ       .BCC
-580  17C2 E5 E0                  SBCZ       .BCAP1
-581  17C4 A6 B1                  LDX       .SQUARE       ***************
-582  17C6 E0 33                  CPX #      33
-583  17C8 F0 16                  BEQ        POSN          POSITION
-584  17CA E0 34                  CPX #      34            BONUS FOR
-585  17CC F0 12                  BEQ        POSN          MOVE TO
-586  17CE E0 22                  CPX #      22            CENTRE
-587  17D0 F0 0E                  BEQ        POSN             OR
-588  17D2 E0 25                  CPX #      25            OUT OF
-589  17D4 F0 0A                  BEQ        POSN          BACK RANK
-590  17D6 A6 B0                  LDX       .PIECE
-591  17D8 F0 09                  BEQ        NOPOSN
-592  17DA B4 50                  LDYZX      .BOARD
-593  17DC C0 10                  CPY #      10
-594  17DE 10 03                  BPL        NOPOSN
-595  17E0 18                     POSN       CLC
-596  17E1 69 02                  ADCIM      02
-597  17E3 4C 77 03     NOPOSN    JMP        CKMATE        CONTINUE
-598                    ;
-599                    ;
-600                    ;
+STRATGY: CLC
+         LDA     #$80
+         ADC     WMOB           ; PARAMETERS
+         ADC     WMAXC          ; WITH WEIGHT
+         ADC     WCC            ; OF 0.25
+         ADC     WCAP1
+         ADC     WCAP2
+         SEC
+         SBC     PMAXC
+         SBC     PCC
+         SBC     BCAPO
+         SBC     BCAP1
+         SBC     BCAP2
+         SBC     PMOB
+         SBC     BMOB
+         BCS     POS            ; UNDERFLOW
+         LDA     #$00           ; PREVENTION
+POS:     LSR     A
+         CLC                    ; **************
+         ADC     #$40
+         ADC     WMAXC          ; PARAMETERS
+         ADC     WCC            ; WITH WEIGHT
+         SEC                    ; OF 0.5
+         SBC     BMAXC
+         LSR     A              ;  **************
+         CLC
+         ADC     #$90
+         ADC     WCAPO          ; PARAMETERS
+         ADC     WCAPO          ; WITH WEIGHT
+         ADC     WCAP0          ; OF 1.0
+         ADC     WCAP0
+         ADC     WCAP1
+         SEC                    ; [UNDER OR OVER-
+         SBC     BMAXC          ; FLOW MAY OCCUR
+         SBC     BMAXC          ; FROM THIS
+         SBC     BBCC            ; SECTION]
+         SBC     BBCC
+         SBC     BCAP1
+         LDX     SQUARE         ; ***************
+         CPX     #$33
+         BEQ     POSN           ; POSITION
+         CPX     #$34           ; BONUS FOR
+         BEQ     POSN           ; MOVE TO
+         CPX     #$22           ; CENTRE
+         BEQ     POSN           ; OR
+         CPX     #$25           ; OUT OF
+         BEQ     POSN           ; BACK RANK
+         LDX     PIECE
+         BEQ     NOPOSN
+         LDY     BOARD,X
+         CPY     #$10
+         BPL     NOPOSN
+POSN:    CLC
+         ADC     #$02
+NOPOSN:  JMP     CKMATE         ; CONTINUE
+;
+;
+;
