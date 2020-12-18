7ca519e39b0cba3a59833e9fb416c594a1a6b05b
Jeff Tranter <tranter@pobox.com>
2020-12-18 16:04:19 -0500

    Add example.

---
+++ sbc/examples/hello.s
@@ -0,0 +1,20 @@
+; Simple example to print message on serial console.
+
+        MONCOUT = $FF3B         ; ROM character out routine
+        CR      = $0D           ; Carriage return
+        LF      = $0A           ; Line feed
+
+        .org     $1000          ; Start address
+
+        ldy     #0              ; Index into string
+loop:
+        lda     Hello,y         ; Get character from string
+        beq     done            ; Done if null
+        jsr     MONCOUT         ; Output character
+        iny                     ; Update index
+        bne     loop            ; Go back and repeat
+done:
+        jmp     done            ; Loop forever
+
+Hello:
+        .byte   CR, LF, "Hello, world!", CR, LF, $00
