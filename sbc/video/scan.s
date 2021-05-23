40d18486e39e89e3880e45bbe3da4bef675fd82d
Jeff Tranter <tranter@pobox.com>
2021-05-23 17:58:55 -0400

    Porting code to SBC.

--- sbc/video/scan.s
+++ sbc/video/scan.s
@@ -1,10 +1,10 @@
 ; Scan code for Scungy video 1x32 alphanumeric display
 ; JSR/RTS or BRK method.
 
-        .org    $170
+        .org    $2000
 
         .res    30, $A0         ; LDY #$A0
-;       rts
-;       rts
-        rti
-        rti
+        rts
+        rts
+;       rti
+;       rti
--- sbc/video/scungy.s
+++ sbc/video/scungy.s
@@ -1,27 +1,27 @@
 ; Scungy video demonstration software
 ;
 ; Adapted from Fig. 1-14 on page 32 of "Son of Cheap Video" by Don
-;  Lancaster.
+; Lancaster to run on my 6502 SBC.
 ;
-; uP - 6502             Start - JMP 020E  Displayed 0380-039F
-; System - KIM-1 +      Stop - RST        Program Space 0200-023F
-;          Scungy Video                       (64 words)
-;                                         Scan Space - 1780-179F
+; uP - 6502             Start - JMP 100E  Displayed 0380-039F
+; System - 6502 SBC +   Stop - RST        Program Space 1000-1040
+;          Scungy Video                       (65 words)
+;                                         Scan Space - 2000-201F
 ;                                             (32 words)
-;                                         IRQ - 1780 (17FE 80; 17FF 17)
 
-        SCAN = $1780            ; Start address of SCAN PROM
-        RIOT = $1700            ; Start address of 6530 RIOT
+        SCAN = $2000            ; Start address of SCAN PROM
+        VIA  = $8000            ; Start address of 6522 VIA
+        VIA_DDRA = VIA+3        ; DDRA register
+        VIA_ORA  = VIA+1        ; ORA register
 
-        .org    $0200
+        .org    $1000
 
 ; Live Scan Subroutine:
-l0200:  INC     RIOT            ; Output H sync pulse
-        INC     RIOT            ; Advance row count
-        BRK                     ; ///DO SCAN MICROINSTRUCTION///
-        NOP                     ; Equalize 2 uS
+l0200:  INC     VIA_ORA         ; Output H sync pulse
+        INC     VIA_ORA         ; Advance row count
+        JSR     SCAN            ; ///DO SCAN MICROINSTRUCTION///
 
-        CMP     RIOT            ; Is this the last dot row?
+        CMP     VIA_ORA         ; Is this the last dot row?
         BNE     l0200           ; No, do another row of dots
         RTS                     ; Return to main scan
 
@@ -31,23 +31,23 @@ START:  NOP                     ; Equalize 6 uS
         NOP
         NOP
         LDA     #$FF            ; Make A port an output
-        STA     RIOT+1          ;  continued
+        STA     VIA_DDRA        ;  continued
         LDA     #1              ; Start V sync pulse
 
-        STA     RIOT            ;  continued
+        STA     VIA_ORA         ;  continued
         LDA     #$0E            ; Last row compare
         LDY     #$1F            ; Delay for rest of V Sync
 l021f:  DEY                     ;  continued
 
         BNE     l021f           ;  continued
-        DEC     RIOT            ; End V sync pulse
+        DEC     VIA_ORA         ; End V sync pulse
         LDX     #$AF            ; Set # of blank scans
 l0227:  PHA                     ; Equalize 9 us
 
         PLA                     ;  continued
         NOP                     ;  continued
-        INC     RIOT            ; Output H sync pulse
-        DEC     RIOT            ;  continued
+        INC     VIA_ORA         ; Output H sync pulse
+        DEC     VIA_ORA         ;  continued
 
         LDY     #$08            ; Delay to complete blank scan
 l0232:  DEY                     ;  continued
