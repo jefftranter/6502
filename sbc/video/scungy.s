3b6431a960ee392597de7a73afc7e3c0bf9ff565
Jeff Tranter <tranter@pobox.com>
2021-06-05 11:20:09 -0400

    More tweaks.

--- sbc/video/scungy.s
+++ sbc/video/scungy.s
@@ -36,8 +36,7 @@
 ;
 ; TODO:
 ; Define constants for magic numbers.
-; Use 65C02 instructions for 1 cycle NOP.
-; Write some characters the video memory.
+; Initially write some characters to the video memory.
 ; Make version for 2 MHz CPU clock.
 
 
@@ -57,11 +56,13 @@
 
         .org    $1FA0
 
+        .setcpu "65c02"
+
 start:
 
-; Initialize VIA ports PA0-3 as outputs
+; Initialize VIA port A ports as outputs (only use 0-3, but needed so we can read back values)
 
-        LDA     #%00001111      ; (2)
+        LDA     #%11111111      ; (2)
         STA     VIA_DDRA        ; (4)
 
 loop:
@@ -127,7 +128,7 @@ hloop2:
         BNE     bloop2          ; (2+) Go back until done
         NOP1                    ; (1) Use up 1 cycle
 
-        jmp     loop            ; Go back and repeat
+        BRA     loop            ; (3) Go back and repeat
 
         .res   $2000-*,$00
 
