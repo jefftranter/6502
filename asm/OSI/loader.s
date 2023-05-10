ac1b45c0c22a08c63753d84b7d6a59f4d4dcf5e1
Jeff Tranter <tranter@pobox.com>
2023-05-09 20:00:22 -0400

    Check in disassembled program loader code.

---
+++ asm/OSI/loader.s
@@ -0,0 +1,97 @@
+; Serial loader program, found on OSI cassettes for Extended Monitor and Editor/Assembler.
+; This code is followed by the actual program to load in MOS Technology format.
+
+CR       = $0D                  ; Carriage Return
+LF       = $0A                  ; Line Feed
+LDFLAG   = $0203                ; BASIC load flag 00=no load, FF-load from ACIA
+INVEC    = $FFEB                ; Input routine
+OUTVEC   = $FFEE                ; Output Routine
+
+        .org    $0700           ; Address varies depending on program to be loaded
+
+        .word   $0000
+ADDR    .word   $0000           ; Holds start address of loaded program
+D0704   .byte   $00
+
+START   jsr     SUB1            ; Get input byte
+        cmp     #';'            ; Line should start with ';'
+        beq     LINE            ; If so, branch
+        cmp     #'$'            ; Last line should be dollar sign followed by start address, e.g. "$0800"
+        bne     START           ; If not, keep reading.
+        jsr     S0781           ; Get hex byte from input
+        sta     ADDR+1          ; Store high byte
+        jsr     S0781           ; Get hex byte form input
+        sta     ADDR            ; Store low byte
+        jmp     GO
+LINE    lda     #$00            ; Initialize checksum to zero
+        sta     ADDR
+        sta     ADDR+1
+        jsr     S076E
+        tax
+        jsr     S076E
+        sta     STORE+2
+        jsr     S076E
+        sta     STORE+1
+L0737   jsr     S076E
+STORE   sta     $0FFF           ; Address changed at run-time
+        inc     STORE+1
+        bne     L0745
+        inc     STORE+2
+L0745   dex
+        bne     L0737
+        jsr     S0781
+        cmp     ADDR+1
+        bne     L0758
+        jsr     S0781
+        cmp     ADDR
+        beq     START
+L0758   ldy     #$00
+PRNT    lda     ERRMSG,y
+        beq     L0765
+        jsr     OUTVEC
+        iny
+        bne     PRNT
+L0765   jsr     S07A3
+        cmp     #'G'
+        bne     L0765
+        beq     START
+S076E   jsr     S0781
+        clc
+        adc     ADDR
+        sta     ADDR
+        bcc     L077D
+        inc     ADDR+1
+L077D   lda     D0704
+        rts
+S0781   jsr     S0784
+S0784   jsr     SUB1
+        cmp     #'A'            ; Check for hex digit (A-F)
+        bcc     L078D           ; Branch if less
+        sbc     #$07            ; Adjust for hex digit
+L078D   and     #$0F            ; Mask out ASCII bits
+        asl     a               ; Shift into upper nybble
+        asl     a
+        asl     a
+        asl     a
+        ldy     #$04
+L0795   rol     a
+        rol     D0704
+        dey
+        bne     L0795
+        lda     D0704
+        rts
+SUB1    lda     #$80
+        .byte   $2C          ; BIT fallthrough trick
+S07A3   lda     #$00
+        sta     LDFLAG
+        jsr     INVEC
+        jmp     OUTVEC
+GO      lda     #$00         ; Turn off cassette/serial load flag
+        sta     LDFLAG
+        jmp     (ADDR)       ; Jump to loaded program start
+
+ERRMSG  .byte   CR,LF,LF
+        .byte   "OBJ CHECKSUM ERR"
+        .byte   CR,LF
+        .byte   "REWIND PAST ERR - TYPE G TO RESTART"
+        .byte   $00
