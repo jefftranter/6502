61207169de44608dd691fde5880dc9109f3b23c2
Jeff Tranter <tranter@pobox.com>
2024-02-20 17:39:26 -0500

    Check in version of WozMon that works with serial interface.

---
+++ asm/wozmon/wozmon-serial.s
@@ -0,0 +1,169 @@
+;  The WOZ Monitor for the Apple 1
+;  Written by Steve Wozniak in 1976
+
+;  This is a special version for the Briel Replica 1 that uses the
+;  serial interface on the Multi I/O board.
+;  Keep in mind that once you start an Apple 1 program that was
+;  downloaded via the serial Woz monitor that the default display (TV
+;  and keyboard) will be used instead of the serial Woz.
+;  Set your terminal program to 19200 BAUD 8 bit 1 stop no parity.
+;  When you start the program, hit ESC on your PC computer to establish
+;  communication.
+
+
+; Page 0 Variables
+
+XAML            = $24           ; Last "opened" location Low
+XAMH            = $25           ; Last "opened" location High
+STL             = $26           ; Store address Low
+STH             = $27           ; Store address High
+L               = $28           ; Hex value parsing Low
+H               = $29           ; Hex value parsing High
+YSAV            = $2A           ; Used to see if hex value is given
+MODE            = $2B           ; $00=XAM, $7F=STOR, $AE=BLOCK XAM
+
+
+; Other Variables
+
+IN              = $0200         ; Input buffer to $027F
+DATA            = $C300         ; 6551 ACIA Data Register
+STATUS          = $C301         ; 6551 ACIA Status Register
+CMD             = $C302         ; 6551 ACIA Command Register
+CTRL            = $C303         ; 6551 ACIA Control Register
+
+
+; Two possible addressing options - select one
+               .org $7F00       ; In the 32K RAM area at 7F00-7FFF
+;              .org $A000       ; In the EEPROM on the multi I/O board at A000-A0FF
+
+               .export RESET
+
+RESET:          CLD             ; Clear decimal arithmetic mode.
+                CLI
+                LDA #$1F
+                STA CTRL        ; Initialize ACIA
+                LDA #$0B
+                STA CMD
+NOTCR:          CMP #'_'+$80    ; "_"?
+                BEQ BACKSPACE   ; Yes.
+                CMP #$9B        ; ESC?
+                BEQ ESCAPE      ; Yes.
+                INY             ; Advance text index.
+                BPL NEXTCHAR    ; Auto ESC if > 127.
+ESCAPE:         LDA #'\'+$80    ; "\".
+                JSR ECHO        ; Output it.
+GETLINE:        LDA #$8D        ; CR.
+                JSR ECHO        ; Output it.
+                LDY #$01        ; Initialize text index.
+BACKSPACE:      DEY             ; Back up text index.
+                BMI GETLINE     ; Beyond start of line, reinitialize.
+NEXTCHAR:       LDA STATUS      ; Key ready?
+                AND #$08
+                BEQ NEXTCHAR    ; Loop until ready.
+                LDA DATA        ; Load character.
+                ORA #$80        ; Set B7.
+                STA IN,Y        ; Add to text buffer.
+                JSR ECHO        ; Display character.
+                CMP #$8D        ; CR?
+                BNE NOTCR       ; No.
+                LDY #$FF        ; Reset text index.
+                LDA #$00        ; For XAM mode.
+                TAX             ; 0->X.
+SETSTOR:        ASL             ; Leaves $7B if setting STOR mode.
+SETMODE:        STA MODE        ; $00=XAM, $7B=STOR, $AE=BLOCK XAM.
+BLSKIP:         INY             ; Advance text index.
+NEXTITEM:       LDA IN,Y        ; Get character.
+                CMP #$8D        ; CR?
+                BEQ GETLINE     ; Yes, done this line.
+                CMP #'.'+$80    ; "."?
+                BCC BLSKIP      ; Skip delimiter.
+                BEQ SETMODE     ; Set BLOCK XAM mode.
+                CMP #':'+$80    ; ":"?
+                BEQ SETSTOR     ; Yes. Set STOR mode.
+                CMP #'R'+$80    ; "R"?
+                BEQ RUN         ; Yes. Run user program.
+                STX L           ; $00->L.
+                STX H           ;  and H.
+                STY YSAV        ; Save Y for comparison.
+NEXTHEX:        LDA IN,Y        ; Get character for hex test.
+                EOR #$B0        ; Map digits to $0-9.
+                CMP #$0A        ; Digit?
+                BCC DIG         ; Yes.
+                ADC #$88        ; Map letter "A"-"F" to $FA-FF.
+                CMP #$FA        ; Hex letter?
+                BCC NOTHEX      ; No, character not hex.
+DIG:            ASL
+                ASL             ; Hex digit to MSD of A.
+                ASL
+                ASL
+                LDX #$04        ; Shift count.
+HEXSHIFT:       ASL             ; Hex digit left, MSB to carry.
+                ROL L           ; Rotate into LSD.
+                ROL H           ; Rotate into MSD’s.
+                DEX             ; Done 4 shifts?
+                BNE HEXSHIFT    ; No, loop.
+                INY             ; Advance text index.
+                BNE NEXTHEX     ; Always taken. Check next character for hex.
+NOTHEX:         CPY YSAV        ; Check if L, H empty (no hex digits).
+                BEQ ESCAPE      ; Yes, generate ESC sequence.
+                BIT MODE        ; Test MODE byte.
+                BVC NOTSTOR     ; B6=0 STOR, 1 for XAM and BLOCK XAM
+                LDA L           ; LSD’s of hex data.
+                STA (STL,X)     ; Store at current ‘store index’.
+                INC STL         ; Increment store index.
+                BNE NEXTITEM    ; Get next item. (no carry).
+                INC STH         ; Add carry to ‘store index’ high order.
+TONEXTITEM:     JMP NEXTITEM    ; Get next command item.
+RUN:            JMP (XAML)      ; Run at current XAM index.
+NOTSTOR:        BMI XAMNEXT     ; B7=0 for XAM, 1 for BLOCK XAM.
+                LDX #$02        ; Byte count.
+SETADR:         LDA L-1,X       ; Copy hex data to
+                STA STL-1,X     ;  ‘store index’.
+                STA XAML-1,X    ; And to ‘XAM index’.
+                DEX             ; Next of 2 bytes.
+                BNE SETADR      ; Loop unless X=0.
+NXTPRNT:        BNE PRDATA      ; NE means no address to print.
+                LDA #$8D        ; CR.
+                JSR ECHO        ; Output it.
+                LDA XAMH        ; ‘Examine index’ high-order byte.
+                JSR PRBYTE      ; Output it in hex format.
+                LDA XAML        ; Low-order ‘examine index’ byte.
+                JSR PRBYTE      ; Output it in hex format.
+                LDA #':'+$80    ; ":".
+                JSR ECHO        ; Output it.
+PRDATA:         LDA #$A0        ; Blank.
+                JSR ECHO        ; Output it.
+                LDA (XAML,X)    ; Get data byte at ‘examine index’.
+                JSR PRBYTE      ; Output it in hex format.
+XAMNEXT:        STX MODE        ; 0->MODE (XAM mode).
+                LDA XAML
+                CMP L           ; Compare ‘examine index’ to hex data.
+                LDA XAMH
+                SBC H
+                BCS TONEXTITEM  ; Not less, so no more data to output.
+                INC XAML
+                BNE MOD8CHK     ; Increment ‘examine index’.
+                INC XAMH
+MOD8CHK:        LDA XAML        ; Check low-order ‘examine index’ byte
+                AND #$0F        ;  For MOD 8=0
+                BPL NXTPRNT     ; Always taken.
+PRBYTE:         PHA             ; Save A for LSD.
+                LSR
+                LSR
+                LSR             ; MSD to LSD position.
+                LSR
+                JSR PRHEX       ; Output hex digit.
+                PLA             ; Restore A.
+PRHEX:          AND #$0F        ; Mask LSD for hex print.
+                ORA #'0'+$80    ; Add "0".
+                CMP #$BA        ; Digit?
+                BCC ECHO        ; Yes, output it.
+                ADC #$06        ; Add offset for letter.
+ECHO:           PHA
+                AND #$7F
+                STA DATA
+POLL:           LDA STATUS
+                AND #$10
+                BEQ POLL        ; No, wait for display.
+                PLA
+                RTS             ; Return.
